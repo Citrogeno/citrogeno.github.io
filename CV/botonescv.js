0dfb9ccbe015f2957e8947e94281c39f723190ea
Citrogeno <126626783+Citrogeno@users.noreply.github.com>
2023-02-28 22:55:37 -0300

    Add files via upload

---
+++ CV/botonescv.js
@@ -0,0 +1,10 @@
+
+document.getElementById("botoni").onclick = function(){
+    document.getElementById("redes").innerHTML = "@Mantequitakillemall";
+    }
+    document.getElementById("botonf").onclick = function(){
+        document.getElementById("redes").innerHTML = "@Regaladoescaro";
+    }
+    document.getElementById("botont").onclick = function(){
+        document.getElementById("redes").innerHTML = "@Asesinoabajosueldo";
+    }
